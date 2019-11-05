0259f487872447c7288a449da95604fe194319dd
admin <admin@qq.com>
2019-11-05 16:36:56 +0800

    d11

---
+++ xcEduService/xc-framework-parent/xc-service-api/src/main/java/com/xuecheng/api/search/EsCourseControllerApi.java
@@ -0,0 +1,20 @@
+package com.xuecheng.api.search;
+
+import com.xuecheng.framework.domain.course.CoursePub;
+import com.xuecheng.framework.domain.search.CourseSearchParam;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * @author hewei
+ * @date 2019/11/5 - 14:20
+ */
+@Api(value = "课程搜索",description = "课程搜索",tags = {"课程搜索"})
+public interface EsCourseControllerApi {
+    //搜索课程信息
+    @ApiOperation("课程综合搜索")
+    public QueryResponseResult<CoursePub> list(int page, int size, CourseSearchParam courseSearchParam);
+
+
+}
---
+++ xcEduService/xc-framework-parent/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CoursePubRepository.java
@@ -0,0 +1,11 @@
+package com.xuecheng.manage_course.dao;
+
+import com.xuecheng.framework.domain.course.CoursePub;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author hewei
+ * @date 2019/11/5 - 3:54
+ */
+public interface CoursePubRepository extends JpaRepository<CoursePub,String> {
+}
